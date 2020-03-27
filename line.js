e88fed93f7e13a64b3b89b10fb4803224b66ab6a
Michael Lawrence Evans <mlevans@users.noreply.github.com>
2020-03-27 14:20:32 -0400

    Create line.js

---
+++ line.js
@@ -0,0 +1,20 @@
+var d3 = require("d3");
+var turf = require("@turf/turf")
+var fs = require('fs');
+
+
+//var data = fs.readFileSync('stops.csv');
+
+fs.readFile("stops.csv", "utf8", function(error, data) {
+  data = d3.csvParse(data);
+  console.log(data);
+
+  var line =[]
+  for (var i=0; i<data.length; i++){
+    line.push([+data[i].lng, +data[i].lat])
+  }
+
+  var geo_line = turf.lineString(line);
+  console.log(geo_line);
+  fs.writeFileSync('bu_route.geojson', JSON.stringify(geo_line));
+});
